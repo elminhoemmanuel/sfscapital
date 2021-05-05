56db6b9b1704cf2facac4a944cb9f09ecc6fcb51
elminhoemmanuel <amodue15@gmail.com>
2021-05-04 22:42:29 -0600

    made adjustments to testimonials section

--- components/TestimonialsCard.js
+++ components/TestimonialsCard.js
@@ -7,8 +7,8 @@ const TestimonialCard = ({ testimonialObj }) => {
                         <img className=' object-cover relative z-30 rounded-full w-24 h-24 shadow-xl border-2 border-gray-200' src="/images/passport.jpg" alt="testifier picture"/>
                     </div>
             </div>
-            <div className='shadow-xl px-6 pt-20 pb-10 bg-white follower'>
-                <div className='flex flex-col w-full p-8'>
+            <div className='shadow-xl px-4 md:px-6 py-10 bg-white follower'>
+                <div className='flex flex-col w-full  py-8'>
                     <div className='mb-2 text-sm text-didallatitle'><p>{testimonialObj.text}</p></div>
                     <div className='mb-2 text-sm'><p className='font-bold'>{testimonialObj.name}</p></div>
                     <div className='mb-2 text-sm text-didallabody'><p className=''>{testimonialObj.location}</p></div>
