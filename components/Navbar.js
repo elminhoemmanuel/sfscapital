0165168073d6dc6d21643439d044170372c85182
elminhoemmanuel <amodue15@gmail.com>
2021-08-31 16:40:36 -0700

    finished landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
 
             {/* mobile screens vertical nav */}
             <div className={menuShow ? 'mobile-box-show flex shadow-xl flex-col lg:hidden bg-nav text-white w-full h-full fixed top-0 z-50 pt-8 max-w-screen-2xl transition'
-                : 'mobile-box flex shadow-xl flex-col lg:hidden bg-nav text-white w-3/4 h-full fixed top-0  z-50 pt-8 max-w-screen-2xl '}>
+                : 'mobile-box flex shadow-xl flex-col lg:hidden bg-nav text-white w-0 h-full fixed top-0  z-50 pt-8 max-w-screen-2xl '}>
                 <div className=''>
                     <div className='lg:hidden flex flex-row justify-end mb-6 px-2'>
                         <button onClick={handleClickMobile} className='block focus:outline-none outline-none' type='button'><svg className="w-8 h-8 text-white hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg></button>
@@ -52,7 +52,7 @@ const Navbar = () => {
                     </div>
 
                     <div className="mb-4 text-center">
-                        <PriBtn btnType='button' btnText="Join" addStyle="font-bold text-base px-60" />
+                        <PriBtn btnType='button' btnText="Join" addStyle="font-bold text-base px-12" />
                     </div>
 
 
@@ -90,7 +90,7 @@ const Navbar = () => {
                         <PriBtn btnType='button' btnText="Join" addStyle="font-bold text-base px-8" />
                     </div>
 
-                    <div className='lg:hidden'>
+                    <div className='md:hidden'>
                         <button onClick={handleClick} className='block focus:outline-none outline-none' type='button'><svg className="w-8 h-8 text-white hover:text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg></button>
                     </div>
                 </div>
