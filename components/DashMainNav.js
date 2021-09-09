a6a4c7c7d3556554626a0c63997ba0bff8193f0c
elminhoemmanuel <amodue15@gmail.com>
2021-09-09 09:02:00 -0700

    made a fix for border on top nav on dashboard

--- components/DashMainNav.js
+++ components/DashMainNav.js
@@ -89,31 +89,33 @@ const DashMainNav = () => {
 
             </div>
 
-            <div className='bg-transparent px-6 md:px-8 py-3 border-b border-navborder flex items-center justify-between'>
-                <div className="flex items-center">
-                    <div className="mr-2 md:hidden">
-                        <button onClick={handleClick} className="focus:outline-none block w-full">
-                            <GrMenu className="h-6 w-6 text-sfsblue" />
-                        </button>
-                    </div>
-                    <div className="rounded-full py-2 px-5 md:px-8 lg:px-16 bg-footeryellow text-sm text-white">
-                        <p>Hi Andie! you are yet to complete your account setup</p>
-                    </div>
-                </div>
-                <div className="md:flex items-center hidden">
-                    <div>
-                        <Link href="/dashboard">
-                            <a className='block w-full mr-3'>
-                                <Image width="40%" height="40%" src="/images/andie.svg" alt="user avatar" className="" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="flex items-center ">
-                        <div className='text-sfsblue text-sm'>
-                            Andie Kane
+            <div className="px-6 md:px-8">
+                <div className='bg-transparent  py-3 border-b border-navborder flex items-center justify-between'>
+                    <div className="flex items-center">
+                        <div className="mr-2 md:hidden">
+                            <button onClick={handleClick} className="focus:outline-none block w-full">
+                                <GrMenu className="h-6 w-6 text-sfsblue" />
+                            </button>
                         </div>
-                        <div className='text-sfsblue text-sm'>
-                            <RiArrowDropDownLine className='h-5 w-5' />
+                        <div className="rounded-full py-2 px-5 md:px-8 lg:px-16 bg-footeryellow text-sm text-white">
+                            <p>Hi Andie! you are yet to complete your account setup</p>
+                        </div>
+                    </div>
+                    <div className="md:flex items-center hidden">
+                        <div>
+                            <Link href="/dashboard">
+                                <a className='block w-full mr-3'>
+                                    <Image width="40%" height="40%" src="/images/andie.svg" alt="user avatar" className="" />
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="flex items-center ">
+                            <div className='text-sfsblue text-sm'>
+                                Andie Kane
+                            </div>
+                            <div className='text-sfsblue text-sm'>
+                                <RiArrowDropDownLine className='h-5 w-5' />
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/SideNav.js
+++ components/SideNav.js
@@ -5,7 +5,7 @@ import { sideData } from './SideNavData'
 
 const SideNav = () => {
     return (
-        <div className='overflow-y-hidden bg-sfsblue side'>
+        <div className='side'>
             <div className="fixed w-60 overflow-y-hidden h-screen bg-sfsblue text-dashtext p-4 flex flex-col justify-between">
                 <div className="">
                     <div className='px-3 pt-5 pb-5'>
